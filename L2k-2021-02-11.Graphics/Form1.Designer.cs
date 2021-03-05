37d0820ad552040f6a569dcf7f128797cf6d971b
Smak80 <smaklets@kpfu.ru>
2021-03-05 10:24:09 +0300

    Добавьте файлы проекта.

---
+++ L2k-2021-02-11.Graphics/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace L2k_2021_02_11.Graphics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPanel.BackColor = System.Drawing.Color.White;
+            this.mainPanel.Location = new System.Drawing.Point(13, 13);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(526, 471);
+            this.mainPanel.TabIndex = 0;
+            this.mainPanel.SizeChanged += new System.EventHandler(this.mainPanel_SizeChanged);
+            this.mainPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.mainPanel_Paint);
+            this.mainPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainPanel_MouseDown);
+            this.mainPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mainPanel_MouseMove);
+            this.mainPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.mainPanel_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 496);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+    }
+}
+
---
+++ L2k-2021-02-11.Graphics/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L2k_2021_02_11.Graphics
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mainPanel_Paint(object sender, PaintEventArgs e)
+        {
+            System.Drawing.Graphics g = e.Graphics;
+            //System.Drawing.Graphics g = mainPanel.CreateGraphics();
+            Pen pen = new Pen(Color.Red, 5);
+            g.DrawRectangle(pen, 10, 10, 300, 200);
+            //Brush brush = new SolidBrush(Color.Blue);
+            Rectangle r = new Rectangle(10, 10, 300, 200);
+            Brush brush = new LinearGradientBrush(r,
+                Color.Red,
+                Color.Blue,
+                45);
+            g.FillEllipse(brush, 10, 400, 300, 200);
+        }
+
+        private void mainPanel_SizeChanged(object sender, EventArgs e)
+        {
+            mainPanel.Invalidate();
+        }
+
+        private Point lastPos = new Point(0, 0);
+        private bool pressed = false;
+        private Color c;
+
+        private void mainPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPos.X = e.X;
+            lastPos.Y = e.Y;
+            pressed = true;
+            if (e.Button == MouseButtons.Left)
+            {
+                c = Color.Green;
+            } else if (e.Button == MouseButtons.Right)
+            {
+                c = Color.Aqua;
+            }
+        }
+
+        private void mainPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            pressed = false;
+        }
+
+        private void mainPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (pressed)
+            {
+                System.Drawing.Graphics g = mainPanel.CreateGraphics();
+                Pen p = new Pen(c, 2);
+                g.DrawLine(p, lastPos, e.Location);
+                lastPos = e.Location;
+            }
+        }
+    }
+}
